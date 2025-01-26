0a1764e121e5349f764172569fcea2e2f38e596d
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-01-26 07:39:00 +0000

    Made program to outout name, pursued degree, and hobby

---
+++ Module 1/M1T2_Brunson.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1T2
+// Your Name
+// 1/26/25
+
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+   cout << "Ricky Brunson" << endl;
+   cout << "Associate In Engineering" << endl;
+   cout << "I like to play instruments" << endl;
+
+   return 0;
+}
