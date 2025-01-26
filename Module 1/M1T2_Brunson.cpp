cacee1eed41dc8df7e1d8477de951302a84f0442
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-01-26 07:47:05 +0000

    Idk whats happening anymore

--- Module 1/M1T2_Brunson.cpp
+++ Module 1/M1T2_Brunson.cpp
@@ -11,7 +11,7 @@ int main()
 {
    cout << "Ricky Brunson" << endl;
    cout << "Associate In Engineering" << endl;
-   cout << "I like to play instruments" << endl;
+   cout << "I like to play instruments, such as the French Horn" << endl;
 
    return 0;
 }
