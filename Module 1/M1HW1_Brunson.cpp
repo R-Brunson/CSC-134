af66a810201978b4ea0557fe910bc71575c7f680
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-01-27 16:52:15 +0000

    Almost finished code, need to add facts/extra info for the movie

--- Module 1/M1HW1_Brunson.cpp
+++ Module 1/M1HW1_Brunson.cpp
@@ -9,11 +9,17 @@ using namespace std;
   
 int main()
 {
-    string movieName = "WALL-E";
+    string movieName = "\"WALL-E\"";
     string publishYear = "2008";
-    double movieGross = 521.3
+    double movieGross = 521.3;
 
-    
+    cout << "The movie " << movieName;
+    cout << " came out in " << publishYear;
+    cout << ", and had a worldwide gross of around $" << movieGross;
+    cout << "million dollars." << endl;
 
+    //need to add facts, ideas are the studio who released them, ratings, ect
+
+    cout << endl; 
     return 0; // no errors
 }
