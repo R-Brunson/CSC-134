0d49e1b5e28da6823b1606644a1f81b46e70321c
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-01-29 16:43:37 +0000

    started M2T2

---
+++ Module 2/M2T2_Brunson.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M2T2 - Receipt Calculator
+// Ricky Brunson
+// 1/29/25
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() // simple receipt calculator
+{
+// Variables
+double mealPrice = 5.99; // Price of meal
+double taxRate = 0.08; // 8% tax rate
+double taxAmount = 
+
+
+
+return 0;
+}
