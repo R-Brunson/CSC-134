3ad46992b7cb30c5534e40f8b446846af6891e84
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-05 16:42:33 +0000

    started M2LAB

---
+++ Module 2/M2LAB1_Brunson.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M2LAB1 - Case File
+// Ricky Brunson
+// 2/5/2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() // Program is made to calculate volume, cost, customer price, and profit
+{
+// Constants for how much it costs to produce and how much its sold for
+    const double COST_PCF = 0.23; // PCF = Per cubic foot
+    const double CHARGE_PCF = 0.5; 
+
+// Variables for 
+
+
+
+}
