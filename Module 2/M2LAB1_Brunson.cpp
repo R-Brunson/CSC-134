de252f5960713c906fcedabd49bb82bec264c2cb
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-10 15:59:49 +0000

    Finished M2LAB1

--- Module 2/M2LAB1_Brunson.cpp
+++ Module 2/M2LAB1_Brunson.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M2LAB1 - Case File
+// M2LAB1 - Crate Calculator
 // Ricky Brunson
 // 2/5/2025
 
@@ -11,10 +11,42 @@ int main() // Program is made to calculate volume, cost, customer price, and pro
 {
 // Constants for how much it costs to produce and how much its sold for
     const double COST_PCF = 0.23; // PCF = Per cubic foot
-    const double CHARGE_PCF = 0.5; 
+    const double CHARGE_PCF = 0.5; //How much we charge the customer per cost 
 
-// Variables for 
+// Variables for box calculations
+    double length, width, height;
+    double volume;
+    double cost;
+    double charge;
+    double profit;
 
+// Prompt to input crate's dimensions
+    cout << "Crate Financial Analysis Calculator" << endl;
+    cout << endl;
+    cout << "Enter the dimensions of the crate (in feet):" << endl;
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
 
+// Calculation of volume, cost to make, how much you charge, and profit
+    volume = length * width * height;
+    cost = volume * COST_PCF;
+    charge = volume * CHARGE_PCF;
+    profit = charge - cost;
 
+    cout << setprecision(2) << fixed;
+
+// Print results
+    cout << endl;
+    cout << "The volume of the crate is " << volume << " cubic feet." << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << "Cost to Build:\t\t$" << cost << endl;
+    cout << "Customer's Price:\t$" << charge << endl;
+    cout << "Profit:\t\t\t$" << profit << endl;
+
+cout << endl;
+return 0;
 }
