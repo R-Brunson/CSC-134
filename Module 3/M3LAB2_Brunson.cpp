476cda7dd6f35e0f5a73e7de78da0d9f61476ac3
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-26 15:29:29 +0000

    started M3Lab2

---
+++ Module 3/M3LAB2_Brunson.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+// M3LAB2 - Number to Letter Grades
+// Ricky Brunson
+// 2/26/25
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+  
+int main()
+{
+// Define variables
+    int num_grade; // 0-100
+    string letter_grade; // A, B, C, D, F
+
+// Ask for the number grade
+    cout << "Number grade to letter grade conversion program" << endl;
+    cout << "Enter the number grade: ";
+    cin >> num_grade;
+
+// find the letter grade 
+
+    
+// Tell user the answer
+cout << "The grade " << num_grade << " is a " << letter_grade << endl;
+
+
+
+
+
+
+
+}
