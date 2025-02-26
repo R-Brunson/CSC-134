c54bdaab593b2d2b95ad5c0e21bbc977c8a9bdf7
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-26 15:59:20 +0000

    M3LAB finished

--- Module 3/M3LAB2_Brunson.cpp
+++ Module 3/M3LAB2_Brunson.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
 // Define variables
-    int num_grade; // 0-100
+    double num_grade; // 0-100
     string letter_grade; // A, B, C, D, F
 
 // Ask for the number grade
@@ -19,16 +19,29 @@ int main()
     cout << "Enter the number grade: ";
     cin >> num_grade;
 
-// find the letter grade 
-
+// find the letter grade (&& is and) (|| is or)
+    if (num_grade >= 100) {
+    letter_grade = "A+";
+    }
+    else if (num_grade >= 90 && num_grade < 100) {
+        letter_grade = "A";
+    }
+    else if (num_grade >= 80 && num_grade < 90) {
+        letter_grade = "B";
+    }
+    else if (num_grade >= 70 && num_grade < 80) {
+        letter_grade = "C";
+    }
+    else if (num_grade >= 60 && num_grade < 70) {
+        letter_grade = "D";
+    }
+    else if (num_grade >= 0 && num_grade < 60) {
+        letter_grade = "F";
+    }
     
 // Tell user the answer
-cout << "The grade " << num_grade << " is a " << letter_grade << endl;
+    cout << "The grade " << num_grade << " is a " << letter_grade << endl;
+    cout << endl;
 
-
-
-
-
-
-
+return 0;
 }
