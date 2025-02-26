887db2e6b64f6eca8decb1741d5c4522ca79efc9
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-26 16:15:07 +0000

    Formatted M3HW1

---
+++ Module 3/M3HW1_Brunson.cpp
@@ -0,0 +1,38 @@
+// CSC 134
+// M3HW1 - Gold
+// Ricky Brunson
+// 2/26/2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+// QUESTION 1 - Chat Bot - <-----------------------------------------------
+    cout << "Question 1" << endl;
+    cout << endl;
+
+
+
+// QUESTION 2 - Receipt Calculator - <-----------------------------------------------
+cout << "Question 2" << endl;
+cout << endl;
+
+
+
+// QUESTION 3 - CYOA - <-----------------------------------------------
+cout << "Question 3" << endl;
+cout << endl;
+
+
+
+// QUESTION 4 - Math Practice - <-----------------------------------------------
+cout << "Question 4" << endl;
+cout << endl;
+
+
+
+
+    return 0;
+}
