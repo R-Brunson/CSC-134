03290341e4a445ef9c65c0ed25f5ed50e76d8a22
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-02-26 16:33:22 +0000

    Question 1 Done

--- Module 3/M3HW1_Brunson.cpp
+++ Module 3/M3HW1_Brunson.cpp
@@ -10,29 +10,42 @@ using namespace std;
 int main()
 {
 // QUESTION 1 - Chat Bot - <-----------------------------------------------
-    cout << "Question 1" << endl;
+    cout << "Question 1 - Chat Bot" << endl;
     cout << endl;
 
     // Variable
-    string 
+    string input;
 
     // Introduction
-    cout << "Hello, I’m a C++ program!" 
+    cout << "Hello, I'm a C++ program!" << endl;
+    cout << "Do you like me? Please type yes or no. ";
+    cin >> input;
 
+    if (input == "yes") {
+        cout << "That's great! I'm sure we'll get along." << endl;
+    }
+    else if (input == "no") {
+        cout << "Well, maybe you'll learn to like me later." << endl;
+    }
+    else {
+        cout << "If you're not sure… that's OK." << endl;
+    }
+
+    cout << endl;
 // QUESTION 2 - Receipt Calculator - <-----------------------------------------------
-cout << "Question 2" << endl;
+cout << "Question 2 - Receipt Calculator" << endl;
 cout << endl;
 
 
 
 // QUESTION 3 - CYOA - <-----------------------------------------------
-cout << "Question 3" << endl;
+cout << "Question 3 - CYOA" << endl;
 cout << endl;
 
 
 
 // QUESTION 4 - Math Practice - <-----------------------------------------------
-cout << "Question 4" << endl;
+cout << "Question 4 - Math Practice" << endl;
 cout << endl;
 
 
