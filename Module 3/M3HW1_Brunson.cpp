2da5fe09dd8ba92e20e6419681d587f2ae092747
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-03-03 16:47:10 +0000

    Uhhh, Q3 looks done. Havent tested?

--- Module 3/M3HW1_Brunson.cpp
+++ Module 3/M3HW1_Brunson.cpp
@@ -35,64 +35,114 @@ int main()
     cout << endl;
     
 // QUESTION 2 - Receipt Calculator - <-----------------------------------------------
-cout << "Question 2 - Receipt Calculator" << endl;
-cout << endl;
-cout << setprecision(2) << fixed;
+    cout << "Question 2 - Receipt Calculator" << endl;
+    cout << endl;
+    cout << setprecision(2) << fixed;
 
-// Variables
-double meal_price, total_price, tax, dine_in_fee;
-double tax_percent = 0.08; // 8% tax
-double dine_in_tip = 0.15; // mandatory 15% tip
-int choice;
+    // Variables
+    double meal_price, total_price, tax, dine_in_fee;
+    double tax_percent = 0.08; // 8% tax
+    double dine_in_tip = 0.15; // mandatory 15% tip
+    int choice;
 
-// User input
-cout << "How much is the meal before tax? $";
-cin >> meal_price;
-cout << "Was the order to go or dine in? 1 for \"To go\", 2 for \"Dine In\". ";
-cin >> choice;
-cout << endl;
+    // User input
+    cout << "How much is the meal before tax? $";
+    cin >> meal_price;
+    cout << "Was the order to go or dine in? 1 for \"To go\", 2 for \"Dine In\". ";
+    cin >> choice;
+    cout << endl;
 
-// if / else if calculations
-if (choice == 1) {
-    tax = meal_price * tax_percent;
-    total_price = meal_price + tax;
+    // if / else if calculations
+    if (choice == 1) {
+     tax = meal_price * tax_percent;
+        total_price = meal_price + tax;
 
-    cout << "--------------------------" << endl;
-    cout << "Subtotal:\t$" << meal_price << endl;
-    cout << "Tax:\t\t$" << tax << endl;
-    cout << "--------------------------" << endl;
-    cout << "Total:\t\t$" << total_price << endl;
-}
-else if (choice == 2) {
-    tax = meal_price * tax_percent;
-    dine_in_fee = meal_price * dine_in_tip;
-    total_price = meal_price + tax + dine_in_fee;
+        cout << "--------------------------" << endl;
+        cout << "Subtotal:\t$" << meal_price << endl;
+        cout << "Tax:\t\t$" << tax << endl;
+        cout << "--------------------------" << endl;
+        cout << "Total:\t\t$" << total_price << endl;
+    }
+    else if (choice == 2) {
+        tax = meal_price * tax_percent;
+        dine_in_fee = meal_price * dine_in_tip;
+        total_price = meal_price + tax + dine_in_fee;
 
-    cout << "--------------------------" << endl;
-    cout << "Subtotal:\t$" << meal_price << endl;
-    cout << "Tax:\t\t$" << tax << endl;
-    cout << "Dine-In Fee:\t$" << dine_in_fee << endl;
-    cout << "--------------------------" << endl;
-    cout << "Total:\t\t$" << total_price << endl;
-}
-else {
-    cout << "Error. Invalid input.";
-}
+        cout << "--------------------------" << endl;
+        cout << "Subtotal:\t$" << meal_price << endl;
+        cout << "Tax:\t\t$" << tax << endl;
+        cout << "Dine-In Fee:\t$" << dine_in_fee << endl;
+        cout << "--------------------------" << endl;
+        cout << "Total:\t\t$" << total_price << endl;
+    }
+    else {
+     cout << "Error. Invalid input.";
+    }
 
-cout << endl;
+    cout << endl;
 */
 // QUESTION 3 - CYOA - <-----------------------------------------------
-// used Copilot to generate adventure game story and outcomes
-cout << "Question 3 - CYOA" << endl;
-cout << endl;
+    // used Copilot to generate adventure game story and outcomes
+    cout << "Question 3 - CYOA" << endl;
+    cout << endl;
 
-cout << "You are an adventurer exploring a mysterious forest." << endl;
-cout 
+    // Variables
+    string path;
+    int coya_choice;
 
+    // Introduction
+    cout << "You are an adventurer exploring a mysterious forest." << endl;
+    cout << "As you walk deeper into the woods, you come across a fork in the path." << endl;
+    cout << endl;
+    cout << "Left path: This path looks well-trodden and safe." << endl;
+    cout << "Right path: This path is overgrown and looks less traveled." << endl;
+    cout << endl;
+    cout << "Which path do you choose? Left or Right? ";
+    cin >> path;
 
+    // The beginning of the maze of if statements
+    if (path == "left" && path == "Left") { // left path
+        cout << endl;
+        cout << "You walk for a while and come across a sleeping dragon." << endl;
+        cout << endl;
+        cout << "Choice 1: Try to sneak past the dragon." << endl;
+        cout << "Choice 2: Attempt to fight the dragon." << endl;
+        cout << endl;
+        cout << "What action do you take? Choice 1 or 2? ";
+        cin >> coya_choice;
+
+        if (coya_choice == 1) { // option 1 -- left path
+            cout << endl;
+            cout << "Game Over: The dragon wakes up and catches you" << endl;
+        }
+        else if (coya_choice == 2) { // option 2 -- left path
+            cout << endl;
+            cout << "Defeat: The dragon is too strong, and you are defeated." << endl;
+        }
+    }
+    else if (path == "right" && path == "Right") { // right path
+        cout << endl;
+        cout << "You find a hidden treasure chest." << endl;
+        cout << endl;
+        cout << "Choice 1: Open the chest." << endl;
+        cout << "Choice 2: Leave the chest and continue walking." << endl;
+        cout << endl;
+        cout << "What action do you take? Choice 1 or 2? ";
+        cin >> coya_choice;
+
+        if (coya_choice == 1) { // option 1 -- right path
+            cout << endl;
+            cout << "Victory: The chest contains a magical artifact that grants you victory." << endl;
+        }
+        else if (coya_choice == 2) { // option 2 -- right path
+            cout << endl;
+            cout << "Game Over: You miss out on the treasure and get lost in the forest." << endl;
+        } 
+    }
+    
 // QUESTION 4 - Math Practice - <-----------------------------------------------
-cout << "Question 4 - Math Practice" << endl;
-cout << endl;
+    cout << "Question 4 - Math Practice" << endl;
+    cout << endl;
 
 
 
