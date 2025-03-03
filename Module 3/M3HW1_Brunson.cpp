84bd072acf166bac15c3555b519cad9dbfe85b5b
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-03-03 16:14:48 +0000

    finished question 2, started 3

--- Module 3/M3HW1_Brunson.cpp
+++ Module 3/M3HW1_Brunson.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+    /*
 // QUESTION 1 - Chat Bot - <-----------------------------------------------
     cout << "Question 1 - Chat Bot" << endl;
     cout << endl;
@@ -32,26 +33,61 @@ int main()
     }
 
     cout << endl;
+    
 // QUESTION 2 - Receipt Calculator - <-----------------------------------------------
-    cout << "Question 2 - Receipt Calculator" << endl;
-    cout << endl;
-    cout << setprecision(2) << fixed;
+cout << "Question 2 - Receipt Calculator" << endl;
+cout << endl;
+cout << setprecision(2) << fixed;
 
-    // Variables
-    double meal_price, total_price;
-    double tax = 0.08; // 8% tax
-    double dine_in_tip = 0.15; // mandatory 15% tip
+// Variables
+double meal_price, total_price, tax, dine_in_fee;
+double tax_percent = 0.08; // 8% tax
+double dine_in_tip = 0.15; // mandatory 15% tip
+int choice;
 
-    // User input
-    cout << "How much is the meal before tax? $" << endl;
-    cin >> meal_price;
+// User input
+cout << "How much is the meal before tax? $";
+cin >> meal_price;
+cout << "Was the order to go or dine in? 1 for \"To go\", 2 for \"Dine In\". ";
+cin >> choice;
+cout << endl;
 
-    // if / else if calculations
+// if / else if calculations
+if (choice == 1) {
+    tax = meal_price * tax_percent;
+    total_price = meal_price + tax;
 
+    cout << "--------------------------" << endl;
+    cout << "Subtotal:\t$" << meal_price << endl;
+    cout << "Tax:\t\t$" << tax << endl;
+    cout << "--------------------------" << endl;
+    cout << "Total:\t\t$" << total_price << endl;
+}
+else if (choice == 2) {
+    tax = meal_price * tax_percent;
+    dine_in_fee = meal_price * dine_in_tip;
+    total_price = meal_price + tax + dine_in_fee;
+
+    cout << "--------------------------" << endl;
+    cout << "Subtotal:\t$" << meal_price << endl;
+    cout << "Tax:\t\t$" << tax << endl;
+    cout << "Dine-In Fee:\t$" << dine_in_fee << endl;
+    cout << "--------------------------" << endl;
+    cout << "Total:\t\t$" << total_price << endl;
+}
+else {
+    cout << "Error. Invalid input.";
+}
+
+cout << endl;
+*/
 // QUESTION 3 - CYOA - <-----------------------------------------------
+// used Copilot to generate adventure game story and outcomes
 cout << "Question 3 - CYOA" << endl;
 cout << endl;
 
+cout << "You are an adventurer exploring a mysterious forest." << endl;
+cout 
 
 
 // QUESTION 4 - Math Practice - <-----------------------------------------------
