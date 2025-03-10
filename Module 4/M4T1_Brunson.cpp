4028710931733bfe5037476c9bcac51347c9cdc8
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-03-10 14:35:04 +0000

    M4T1 part 2 finished

--- Module 4/M4T1_Brunson.cpp
+++ Module 4/M4T1_Brunson.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M4T1 - saying hi 5 times
+// M4T1 - saying hi 5 times, table of squares
 // Ricky Brunson
 // 3/10/25
 
@@ -9,6 +9,7 @@
 using namespace std;
   
 int main() {
+    // Part 1
     int count = 1;
 
     while (count <= 5) {
@@ -21,6 +22,23 @@ int main() {
 
     cout << endl;
     cout << "Done!" << endl;
+
+    //Part 2
+    const int MIN_NUM = 1;
+    const int MAX_NUM = 10;
+
+    cout << "Table of Squares" << endl;
+    cout << "--------------------" << endl;
+    int num = MIN_NUM;
+    int sq; // num squared
+
+    while (num <= MAX_NUM) {
+        sq = num * num;
+        cout << num << "\t\t" << sq << endl;
+        num++;
+    }
+
+
     return 0;
 }
 
