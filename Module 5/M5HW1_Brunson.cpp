9830a2d1491378d54df84a40f85997c25a6e1e34
R-Brunson <brunsonr5554@student.faytechcc.edu>
2025-04-02 15:48:12 +0000

    Finished question 1

--- Module 5/M5HW1_Brunson.cpp
+++ Module 5/M5HW1_Brunson.cpp
@@ -10,13 +10,15 @@ using namespace std;
 
 // Question declaration
 void question1();
+/*
 void question2();
 void question3();
 void question4();
 void question5();
-void question6();
+*/
 
 int main() {
+    /*
     int homework;
     bool keep_going = true;
 
@@ -24,14 +26,15 @@ int main() {
         cout << endl;
         cout << "M5HW1" << endl;
         cout << endl;
-        cout << "1. Question 1" << endl;
-        cout << "2. Question 2" << endl;
-        cout << "3. Question 3" << endl;
-        cout << "4. Question 4" << endl;
-        cout << "5. Question 4" << endl;
-        cout << "6. Question 4" << endl;
+        cout << "1. Question 1 -- Average Rainfall" << endl;
+        cout << "2. Question 2 -- Volume of a Three Dimensional Hypperrectangle" << endl;
+        cout << "3. Question 3 -- Roman Numerals" << endl;
+        cout << "4. Question 4 -- Geometry Calculator" << endl;
+        cout << "5. Question 5 -- Distance Traveled" << endl;
         cout << "0. Exit" << endl;
-        
+        cout << endl;
+
+
         cin >> homework;
         if (1 == homework) {
             question1();
@@ -60,52 +63,63 @@ int main() {
             cout << endl;
             cout << "Not a valid choice." << endl;
         }
-    }
-    return 0; 
+    } */
+
+
 }
+
 // Question 1
 void question1() {
     string month1, month2, month3;
     double rain1, rain2, rain3;
-    cout << "Enter month: ";
+    double average;
+
+    cout << "Enter the first month: ";
     cin >> month1;
     cout << endl;
     cout << "Enter rainfall for " << month1 << ": ";
     cin >> rain1;
     cout << endl;
 
-    cout << "Enter month: ";
+    cout << "Enter second month: ";
     cin >> month2;
     cout << endl;
     cout << "Enter rainfall for " << month2 << ": ";
     cin >> rain2;
     cout << endl;
 
-    cout << "Enter month: ";
+    cout << "Enter third month: ";
     cin >> month3;
     cout << endl; 
     cout << "Enter rainfall for " << month3 << ": ";
     cin >> rain3;
     cout << endl;
+    
+    cout << setprecision(2) << fixed;
+    average = (rain1 + rain2 + rain3) / 3;
 
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", " << month3 << " is: " << average << endl;
+    cout << endl;
 }
 
 //Question 2
+/*
 void question2() {
-cout << "Hi";
+
 }
 
 //Question 3
 void question3() {
-cout << "Hi";
+
 }
 
 //Question 4
 void question4() {
-cout << "Hi";
+
 }
 
 //Question 5
 void question5() {
-cout << "Hi";
+
 }
+*/
